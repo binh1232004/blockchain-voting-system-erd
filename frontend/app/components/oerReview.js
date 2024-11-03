1f333e36df5bc7ba7b40be4c1d317f5986276757
Binh <binhwork245@gmail.com>
2024-11-03 13:48:30 +0700

    fix: default cover for non-url image in review oer

--- frontend/app/components/oerReview.js
+++ frontend/app/components/oerReview.js
@@ -1,13 +1,15 @@
 'use client'
 import Link from "next/link"
 import Image from "next/image"
+import { isValidUrl } from "../utils"
 export default function OerReview({title, imgUrl, route}){
+    const OER_COVER_DEFAULT  = '/oerTextBookCover.png';
     return (
         <div className="flex flex-row space-x-2 mt-5">
 
             <div className="w-1/2 bg-gray-700 h-[100px]">
                 <Image
-                    src={imgUrl}     
+                    src={isValidUrl( imgUrl ) ? imgUrl : OER_COVER_DEFAULT }     
                     width={100}
                     height={100}
                     alt="Image of open educational text book"
